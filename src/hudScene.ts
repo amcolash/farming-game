d20b53f13e439a8c7495d36097034750272db349
Andrew McOlash <amcolash@gmail.com>
2019-04-23 20:07:57 -0700

    global width/height, sliding farmer bar, other cleanup

--- src/hudScene.ts
+++ src/hudScene.ts
@@ -6,9 +6,12 @@ import { Util } from './util';
 import { FarmerType, Farmer } from './farmer';
 
 export class HUDScene extends Phaser.Scene {
+  isShop: boolean = false;
+  width: number;
+  height: number;
+
+  farmScene: FarmScene;
   registry: Phaser.Data.DataManager;
-  isShop: boolean;
-  farm: Farm;
 
   button: TextButton;
   crop: Phaser.GameObjects.Sprite;
@@ -22,51 +25,49 @@ export class HUDScene extends Phaser.Scene {
   stats: Phaser.GameObjects.Container;
   bars: Phaser.GameObjects.Rectangle[];
   
-  farmScene: FarmScene;
   farmers: Phaser.GameObjects.Container;
   farmerList: Phaser.GameObjects.Container;
+  farmListTarget: number;
 
   constructor() {
     super('HUDScene');
   }
 
   create() {
-    const width: number = Number.parseInt(this.game.config.width.toString());
-    const height: number = Number.parseInt(this.game.config.height.toString());
+    this.width = Number.parseInt(this.game.config.width.toString());
+    this.height = Number.parseInt(this.game.config.height.toString());
 
     this.registry = this.game.registry;
-    this.isShop = false;
 
-    this.add.rectangle(0, height - 100, width, 100, 0x000000).setInteractive({ cursor: 'default' }).setOrigin(0, 0);
-    this.add.rectangle(0, height - 102, width, 2, 0x777777).setOrigin(0, 0);
+    this.add.rectangle(0, this.height - 100, this.width, 100, 0x000000).setInteractive({ cursor: 'default' }).setOrigin(0, 0);
+    this.add.rectangle(0, this.height - 102, this.width, 2, 0x777777).setOrigin(0, 0);
 
-    this.button = new TextButton(this, 20, height - 28, this.isShop ? 'Back' : 'Shop', () => this.toggleShop());
-    this.life = this.add.text(20, height - 90, 'Life: 0');
-    this.profit = this.add.text(20, height - 70, 'Profit: $0');
-    this.crop = this.add.sprite(width / 2 - 8, height - 35, 'crops', Crops[this.registry.get('currentCrop')].frame);
+    this.button = new TextButton(this, 20, this.height - 28, this.isShop ? 'Back' : 'Shop', () => this.toggleShop());
+    this.life = this.add.text(20, this.height - 90, 'Life: 0');
+    this.profit = this.add.text(20, this.height - 70, 'Profit: $0');
+    this.crop = this.add.sprite(this.width / 2 - 8, this.height - 35, 'crops', Crops[this.registry.get('currentCrop')].frame);
     this.crop.setScale(1.5);
     
-    new TextButton(this, width / 2 - 32, height - 100, "<", () => this.game.events.emit('speed', -1));
-    new TextButton(this, width / 2 - 4, height - 100, ">", () => this.game.events.emit('speed', 1));
-    new TextButton(this, width / 2 - 32, height - 80, "-", () => this.game.events.emit('zoom', -1));
-    new TextButton(this, width / 2 - 4, height - 80, "+", () => this.game.events.emit('zoom', 1));
+    new TextButton(this, this.width / 2 - 32, this.height - 100, "<", () => this.game.events.emit('speed', -1));
+    new TextButton(this, this.width / 2 - 4, this.height - 100, ">", () => this.game.events.emit('speed', 1));
+    new TextButton(this, this.width / 2 - 32, this.height - 80, "-", () => this.game.events.emit('zoom', -1));
+    new TextButton(this, this.width / 2 - 4, this.height - 80, "+", () => this.game.events.emit('zoom', 1));
     
-    this.money = this.add.text(width - 20, height - 28, '$0', { fontSize: 24 });
+    this.money = this.add.text(this.width - 20, this.height - 28, '$0', { fontSize: 24 });
     this.money.setOrigin(1, 0);
 
-    this.planted = this.add.text(width - 20, height - 90, 'Planted: 0');
+    this.planted = this.add.text(this.width - 20, this.height - 90, 'Planted: 0');
     this.planted.setOrigin(1, 0);
     this.game.events.addListener('planted', value => this.planted.setText('Planted: ' + value));
 
-    this.speed = this.add.text(width - 20, height - 70, 'Speed: ' + (1 / this.scene.get('FarmScene').physics.world.timeScale).toFixed(0));
+    this.speed = this.add.text(this.width - 20, this.height - 70, 'Speed: ' + (1 / this.scene.get('FarmScene').physics.world.timeScale).toFixed(0));
     this.speed.setOrigin(1, 0);
     this.game.events.addListener('speedValue', value => this.speed.setText('Speed: ' + value));
 
-    this.stats = this.add.container(20, height - 125);
+    this.stats = this.add.container(20, this.height - 125);
     this.stats.add(new Phaser.GameObjects.Rectangle(this, -20, 23, 200, 115, 0x000000, 0.35).setOrigin(0, 1));
     
-    this.farm = (this.game.scene.getScene('FarmScene') as FarmScene).farm;
-    this.value = this.add.text(20, height - 50, 'Farm Value: $0');
+    this.value = this.add.text(20, this.height - 50, 'Farm Value: $0');
 
     this.bars = [];
     const sorted = Crops.sort((a, b) => { return a.frame - b.frame });
@@ -83,17 +84,25 @@ export class HUDScene extends Phaser.Scene {
     }
 
     this.farmScene = (this.game.scene.getScene('FarmScene') as FarmScene);
-    this.farmers = this.add.container(width - 175, 0);
+    this.farmers = this.add.container(this.width, 0);
     this.farmerList = this.add.container(0, 0);
+    this.farmListTarget = this.width;
 
-    const rectangle = new Phaser.GameObjects.Rectangle(this, 0, 0, 175, height - 100, 0x000000, 0.5).setOrigin(0);
+    const toggle = new Phaser.GameObjects.Rectangle(this, -42, this.height - 102, 42, 44, 0x000000, 0.5).setOrigin(0, 1);
+    toggle.setInteractive({ useHandCursor: true });
+    toggle.on('pointerdown', this.toggleFarmers.bind(this));
+    toggle.on('pointerover', () => this.game.events.emit('clearHover'));
+    
+    const rectangle = new Phaser.GameObjects.Rectangle(this, 0, 0, 175, this.height - 102, 0x000000, 0.5).setOrigin(0);
     rectangle.setInteractive();
     rectangle.on('pointerover', () => this.game.events.emit('clearHover'));
-
+    
     this.farmers.add(rectangle);
     this.farmers.add(this.farmerList);
+    this.farmers.add(toggle);
+    this.farmers.add(new Phaser.GameObjects.Sprite(this, -36, this.height - 142, 'farmer_a', 0).setOrigin(0).setAlpha(0.85));
+    
     this.getFarmers();
-
     this.game.events.on('farmerUpdate', this.getFarmers.bind(this));
   }
 
@@ -105,7 +114,10 @@ export class HUDScene extends Phaser.Scene {
     this.value.setText('Farm Value: $' + Util.numberWithCommas(this.getFarmValue()));
 
     this.stats.visible = !this.isShop;
-    this.farmerList.visible = !this.isShop;
+    this.farmers.visible = !this.isShop;
+
+    if (this.farmListTarget > this.farmers.x) this.farmers.x = Phaser.Math.Clamp(this.farmers.x + delta * 0.7, this.width - 175, this.width);
+    else if (this.farmListTarget < this.farmers.x) this.farmers.x = Phaser.Math.Clamp(this.farmers.x - delta * 0.7, this.width - 175, this.width);
     
     if (this.stats.visible) {
       const stats = this.registry.get('stats');
@@ -114,6 +126,14 @@ export class HUDScene extends Phaser.Scene {
         if (total === 0) this.bars[i].height = 2;
         else this.bars[i].height = 2 + 47 * (stats[i] / total);
       }
+    }
+  }
+
+  toggleFarmers(): void {
+    if (this.farmListTarget === this.width) {
+      this.farmListTarget = this.width - 175;
+    } else {
+      this.farmListTarget = this.width;
     }
   }
 
@@ -138,8 +158,8 @@ export class HUDScene extends Phaser.Scene {
   }
 
   getFarmValue(): number {
-    var value = this.farm.plowed.size * 5;
-    this.farm.planted.entries.forEach(t => value += t.crop.revenue);
+    var value = this.farmScene.farm.plowed.size * 5;
+    this.farmScene.farm.planted.entries.forEach(t => value += t.crop.revenue);
 
     return value;
   }
